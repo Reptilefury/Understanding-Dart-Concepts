1f9b97f18e5b882d905234f77218f927fef364f4
Reptilefury <reptilefury@outlook.com>
2021-03-11 05:55:52 -0500

    Created a Register page UI
    created a _showUsername input function

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,76 +1,100 @@
 import 'package:flutter/material.dart';
+
 class RegisterPage extends StatefulWidget {
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  @override
+
+Widget _showTitle() {
+  return Text('Register', style: Theme.of(context).textTheme.headline,);
+
+}
+
+Widget _showUsernameInput() {
+  return  Padding(
+    padding: EdgeInsets.only(top: 20.0),
+    child: TextFormField(
+      decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Username',
+          hintText: 'Enter username, min length 6 ',
+          icon: Icon(Icons.face, color: Colors.grey)),
+    ),
+  );
+
+}
+
+ @override
   Widget build(BuildContext context) {
-    return Scaffold(
+  // final Widget _showTitle = Text('Register', style: Theme.of(context).textTheme.headline);
+
+   return Scaffold(
       appBar: AppBar(
-        title: Text('Register'),
+        title: Text('Register') ,
       ),
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: Center(
-          child: SingleChildScrollView(child: Form(child: Column(
-            children: [
-              Text('Register', style: Theme.of(context).textTheme.headline),
-              Padding(padding: EdgeInsets.only(top: 20.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                  hintText: 'Enter username, min length 6 ',
-                  icon: Icon(Icons.face, color: Colors.grey)
-                ),
-              ),
-              ),
-              Padding(padding: EdgeInsets.only(top: 20.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                      hintText: 'Enter a valid email',
-                      icon: Icon(Icons.mail, color: Colors.grey)
-                  ),
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(top: 20.0),
-                child: TextFormField(
-                  obscureText:true,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter password, min length 6 ',
-                      icon: Icon(Icons.lock, color: Colors.grey)
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20.0),
+          child: SingleChildScrollView(
+            child: Form(
                 child: Column(
-                  children: [
+              children: [
+                _showTitle(),
+                //Text('Register', style: Theme.of(context).textTheme.headline),
+
+
+
+                _showUsernameInput(),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        hintText: 'Enter a valid email',
+                        icon: Icon(Icons.mail, color: Colors.grey)),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: 'Enter password, min length 6 ',
+                        icon: Icon(Icons.lock, color: Colors.grey)),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: Column(
+                    children: [
                       RaisedButton(
-                        child: Text('Submit', style: Theme.of(context).textTheme.body1.copyWith(color: Colors.black )),
+                        child: Text('Submit',
+                            style: Theme.of(context)
+                                .textTheme
+                                .body1
+                                .copyWith(color: Colors.black)),
                         elevation: 8.0,
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0))
-                        ) ,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0))),
                         color: Theme.of(context).primaryColor,
                         onPressed: () => print('submit'),
                       ),
-                    FlatButton(
-                      child: Text('Existing User? Login'),
-
-                      onPressed: () => print('Login'),
-                    )
-                  ],
-                ),
-              )
-            ],
-          )),) ,
+                      FlatButton(
+                        child: Text('Existing User? Login'),
+                        onPressed: () => print('Login'),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )),
+          ),
         ),
       ),
     );
