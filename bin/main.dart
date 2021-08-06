17b0a001dea2a45961040169998356e91f92519a
reptilefury <reptilefury@outlook.com>
2021-08-06 10:33:07 +0100

    Understanding Dart

---
+++ bin/main.dart
@@ -0,0 +1,9 @@
+//import 'package:New_Universe/New_Universe.dart' as New_Universe;
+
+void main(List<String> arguments) {
+  print('Hello world: !');
+  return printHello();
+}
+printHello(){
+  print("Hello world");
+}
---
+++ test/New_Universe_test.dart
@@ -0,0 +1,10 @@
+/*
+import 'package:New_Universe/New_Universe.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
+*/
